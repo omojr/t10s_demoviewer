d981d268b9783fb12b3861b97b2c0de4cb1bcf7b
Mykhailo Osadchyi <mosadchyi@intel471.com>
2020-05-31 16:36:43 +0300

    Return 404 when demo with provided id is absent; close DB connections

--- main.py
+++ main.py
@@ -13,19 +13,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    cursor = get_conn().cursor()
+    conn = get_conn()
+    cursor = conn.cursor()
     cursor.execute('select demo_id, server, map, datetime from demos;')
     demos = cursor.fetchall()
+    conn.close()
     demos.sort(key=operator.itemgetter('datetime'), reverse=True)
     return render_template('index.html', demos=demos)
 
 
 @app.route('/get/<demo_id>')
 def download_demo(demo_id):
-    cursor = get_conn().cursor()
+    conn = get_conn()
+    cursor = conn.cursor()
     cursor.execute('select filepath from demos where demo_id=?', (demo_id,))
-    filepath = cursor.fetchone()['filepath']
+    result = cursor.fetchone()
+    if not result:
+        return "<h2>Demo with such id doesn't exist</h2>", 404
+    filepath = result['filepath']
     filename = filepath.rsplit('/', 1)[1]
+    conn.close()
     return send_file(filepath, as_attachment=True, attachment_filename=filename)
 
 
